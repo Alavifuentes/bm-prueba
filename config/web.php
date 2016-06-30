a38ceb10695dbfc3adddb3015581788303daa05d
Fuentes <alaviserafin@gmail.com>
2016-06-30 15:34:11 -0400

    commit

---
+++ config/web.php
@@ -0,0 +1,79 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Lwdyq5wvarik4-sHzgf5yCeVec950ZS_',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'authUrl' => 'https://www.facebook.com/dialog/oauth?display=popup',
+                    'clientId' => '262188020807438',
+                    'clientSecret' => '203b0ef9bcbf39b0cb0d8dad7b7d1b36',
+                ],
+            ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
+
+if (!YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ views/site/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Api';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1></h1>
+
+        <p class="lead">
+
+        </p>
+
+        <p></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+
+                <p></p>
+
+                <p></p>
+            </div>
+            <div class="col-lg-4">
+              
+                <p></p>
+
+                <p></p>
+            </div>
+            <div class="col-lg-4">
+                <h2></h2>
+
+                <p></p>
+
+                <p></p>
+            </div>
+        </div>
+
+    </div>
+</div>
